1ba5c2821082b59c16d7b023189e9aae9c9b101a
arriba87 <arriba87@gmail.com>
2023-06-16 22:08:19 +0200

    dockerization

--- index.py
+++ index.py
@@ -226,5 +226,5 @@ def display_page(pathname):
 # run the app
 if __name__=='__main__':
   print("Starting the server ...")
-  app.run_server(debug=True, port=8050)
+  app.run_server(host='0.0.0.0', port=8050, debug=False)
   
